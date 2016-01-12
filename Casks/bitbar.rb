a190fb031fad55240a7c13b4e3bf96380894fca9
ding444 <dean@four4four.com>
2016-01-12 09:19:50 -0800

    Update bitbar.rb
    Updated for v1.4

--- Casks/bitbar.rb
+++ Casks/bitbar.rb
@@ -1,10 +1,10 @@
 cask 'bitbar' do
-  version '1.3.1'
+  version '1.4.0'
   sha256 '07f3a138b07eeda6c8ad686626676a8fcda6bd10cd371a37274635bac36b1bf5'
 
-  url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar.app.zip"
+  url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar-v1.4.zip"
   appcast 'https://github.com/matryer/bitbar/releases.atom',
-          :sha256 => 'e470d13330b027827c802884046aacf08d1ed66851f2a1803fa5db180b316e97'
+          :sha256 => 'a5e552e96613b9d8d7caffa53519f640c778bac4174afbc531388362912bffb2'
   name 'BitBar'
   homepage 'https://github.com/matryer/bitbar/'
   license :mit
